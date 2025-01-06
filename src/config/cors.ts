6433d3a8b420b624723cea5e8d67d458a9415a4a
Luca Seara <caularase@gmail.com>
2025-01-06 20:15:55 -0300

    Fixing the cors options

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
-const whitelist = [process.env.FRONTEND_URL]
+const whitelist = [process.env.FRONTEND_URL, undefined]
 
 export const corsConfig = {
     origin: function (origin, callback) {
